6a9e7bed4f8566a2dac9b305369e446d5ee7fa8c
Lucifer <Lucifer@10.12.65.90>
2022-11-21 22:31:14 +0530

    jUT

---
+++ src/jUnitTest/jUnitFunction.java
@@ -0,0 +1,13 @@
+package jUnitTest;
+
+public class jUnitFunction {
+	public int AddNumber1(int num1,int num2){
+		return num1+num2;
+		
+	}
+	public String AddString1(String s1,String s2){
+		return s1+s2;
+		
+	}
+
+}
